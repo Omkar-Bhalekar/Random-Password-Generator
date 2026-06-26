2d7927ab627dc07bef3600226d42817678b9df96
Omkar Bhakekar <omkarbhalekar2003@gmail.com>
2026-06-26 22:42:39 +0530

    Add copy to clipboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
@@ -27,6 +27,17 @@ function App() {
 
   },[length,numberAllowed,charAllowed,setPassword])
 
+
+  // Copy Generated Password Function
+
+  const passwordRef = useRef(null);
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,200);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+    
+
   useEffect(()=>{
     passwordGenerator();
   },[length,numberAllowed,charAllowed,passwordGenerator])
@@ -34,18 +45,19 @@ function App() {
   return (
     <>
     
-    <div className='bg-gray-700 w-1/2 mx-auto my-10 h-32 rounded-2xl flex flex-col justify-center items-center'>
+    <div className='bg-gray-800 w-1/2 mx-auto my-10 h-32 rounded-2xl flex flex-col justify-center items-center'>
      <h4 className='mb-2 mt-2 text-white'>Random Password Generator</h4>
      
-     <div className='flex flex-row w-80 h-1/4 mb-4'>
+     <div className='flex flex-row w-80 h-1/4  mt-3.5 mb-5'>
      <input
      type='text'
      value={password}
-     className='bg-gray-200 w-80 outline-none px-2 my-auto text-orange-500 rounded-l-sm'
+     className='bg-gray-200 w-80 h-8 outline-none px-2 my-auto text-orange-500 rounded-l-sm'
      placeholder='password'
+     ref={passwordRef}
      readOnly
      />
-     <button className='bg-blue-500 text-white text-sm rounded-r-sm h-6 mt-1 px-1 '>Copy</button>
+     <button onClick={copyPasswordToClipboard} className='bg-blue-600 text-white text-sm rounded-r-sm h-8  px-1 hover:bg-blue-700'>Copy</button>
      </div>
      
      
